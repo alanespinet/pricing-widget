d10667c2fa506e7a9df769ee1b5a7eacaf5e4aac
Alan Espinet <alan.espinet@everymundo.com>
2019-05-02 17:24:22 -0400

    cors added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const jsonfile = require('./data/pricing_widgets.json')
 const PORT = process.env.PORT || 4000
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res, next) => {
     res.send('This is send')
 })
