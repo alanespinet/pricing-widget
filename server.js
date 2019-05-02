3040057915d0d27e88ffe5bf66694c001b448250
Alan Espinet <alan.espinet@everymundo.com>
2019-05-02 17:22:08 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const jsonfile = require('./data/pricing_widgets.json')
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('This is send')
+})
+
+app.get('/v1/pw', (req, res, next) => {
+    res.json(jsonfile)
+})
+
+app.listen(PORT, () => { console.log('Server is running') })
